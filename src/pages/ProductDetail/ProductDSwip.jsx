1fe478cd6b313864fe148e1578913c4c6b4dd644
krishnamoorthy3 <krishnamoorthym3009@gmail.com>
2025-02-10 11:02:10 +0530

    zoom

--- src/pages/ProductDetail/ProductDSwip.jsx
+++ src/pages/ProductDetail/ProductDSwip.jsx
@@ -1,31 +1,32 @@
 import { Thumbs } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import "./ProductDswip.css"
 import PropTypes from 'prop-types';
 import Wishlistbtn from "../WishListbtn/Wishlistbtn"
+import ReactImageMagnify from 'react-image-magnify';
 
-const ProductDSwip = ({productDetail,handelWishlist,id,category,wishlistbtncolor}) => {
+const ProductDSwip = ({ productDetail, handelWishlist, id, category, wishlistbtncolor }) => {
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-      // Update window width on resize
-        useEffect(() => {
-            const handleResize = () => {
+    // Update window width on resize
+    useEffect(() => {
+        const handleResize = () => {
             setWindowWidth(window.innerWidth); // Update window width on resize
-            };
-    
-            window.addEventListener("resize", handleResize);
-    
-            // Cleanup listener
-            return () => {
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        // Cleanup listener
+        return () => {
             window.removeEventListener("resize", handleResize);
-            };
-        }, []);
-        console.log(windowWidth);
-        
+        };
+    }, []);
+    console.log(windowWidth);
+
     return (
         <>
             <div className="row ">
@@ -35,31 +36,31 @@ const ProductDSwip = ({productDetail,handelWishlist,id,category,wishlistbtncolor
                         watchSlidesProgress
                         onSwiper={setThumbsSwiper}
                         slidesPerView={productDetail.length}
-                        className={windowWidth>=768 ? "swiper-vertical": "swiper-horizontal" }
+                        className={windowWidth >= 768 ? "swiper-vertical" : "swiper-horizontal"}
                         id='thumb-swip-img'
                         breakpoints={{
                             300: {
-                            slidesPerView: 5,
+                                slidesPerView: 5,
                             },
                             370: {
-                            slidesPerView: 5,
+                                slidesPerView: 5,
 
                             },
                             768: {
-                            slidesPerView: productDetail.length,
+                                slidesPerView: productDetail.length,
                             },
                             1024: {
-                            slidesPerView: productDetail.length,
+                                slidesPerView: productDetail.length,
                             },
                             1100: {
-                            slidesPerView: productDetail.length,
+                                slidesPerView: productDetail.length,
                             },
                         }}
                     >
-                        {productDetail.map((item,index)=>
+                        {productDetail.map((item, index) =>
                             <SwiperSlide key={index}>
                                 <div>
-                                    <img src={item} alt={`product${index}`} className='w-100'  />
+                                    <img src={item} alt={`product${index}`} className='w-100' />
                                 </div>
                             </SwiperSlide>
                         )}
@@ -68,16 +69,29 @@ const ProductDSwip = ({productDetail,handelWishlist,id,category,wishlistbtncolor
                 </div>
                 <div className="col-md-10 wishlist-prp marg-t-2">
                     <Swiper modules={[Thumbs]} thumbs={{ swiper: thumbsSwiper }} id='main-swip-img' >
-                        {productDetail.map((item,index)=>
+                        {productDetail.map((item, index) =>
                             <SwiperSlide key={`product${index}`}>
                                 <div>
-                                    <img src={item} alt={`product${index}`} className='w-100' />
+                                    {/* <img src={item} alt={`product${index}`} className='w-100' /> */}
+                                    <ReactImageMagnify {...{
+                                        smallImage: {
+                                            alt: 'Wristwatch by Ted Baker London',
+                                            isFluidWidth: true,
+                                            src: item
+                                        },
+                                        largeImage: {
+                                            src: item,
+                                            width: 800,
+                                            height: 800
+                                        },
+                                        enlargedImageContainerClassName: "custom-large-image" // Add custom class
+                                    }} />
                                 </div>
                             </SwiperSlide>
                         )
 
                         }
-                        
+
                     </Swiper>
                     <Wishlistbtn handelWishlist={handelWishlist} id={id} category={category} wishlistbtncolor={wishlistbtncolor} />
 
@@ -89,11 +103,11 @@ const ProductDSwip = ({productDetail,handelWishlist,id,category,wishlistbtncolor
         </>
     )
 }
-ProductDSwip.propTypes={
-    productDetail:PropTypes.array,
-    handelWishlist:PropTypes.func,
-    wishlistbtncolor:PropTypes.func,
-    id:PropTypes.number,
-    category:PropTypes.string,
+ProductDSwip.propTypes = {
+    productDetail: PropTypes.array,
+    handelWishlist: PropTypes.func,
+    wishlistbtncolor: PropTypes.func,
+    id: PropTypes.number,
+    category: PropTypes.string,
 }
 export default ProductDSwip
